e1a853ce707ee5fe1d1cc125ccc2868bee7734a5
Aslan <fenixx977@gmail.com>
2023-11-17 17:40:26 +0600

    add condition

--- src/bot/logic/dreams/select.py
+++ src/bot/logic/dreams/select.py
@@ -103,7 +103,7 @@ async def dreams_view_func(dream, message, db):
         text = (f"\n*Тема*: {dream.name}\n"
                 f"*Описание*: {dream.description}\n"
                 f"*Имя автора*: {dream_user.name if dream_user else 'Без имени'} "
-                f"{emoji.emojize(':man:') if dream_user.gender == 'Мужчина' else emoji.emojize(':woman:')}")
+                f"{emoji.emojize(':man:') if dream_user and dream_user.gender == 'Мужчина' else emoji.emojize(':woman:')}")
         if dream.image:
             await message.bot.send_photo(message.chat.id,
                                          types.BufferedInputFile(dream.image,
