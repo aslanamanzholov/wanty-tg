0007e609ec361f614aff0f7629ae4de50f8ed156
Aslan <fenixx977@gmail.com>
2023-12-08 02:38:23 +0500

    some changes

--- src/db/repositories/dreams.py
+++ src/db/repositories/dreams.py
@@ -46,7 +46,7 @@ class DreamRepo(Repository[Dream]):
 
     async def get_dream(self, user_id, offset, limit: int = 1):
         """Get dream"""
-        all_records = (select(self.type_model).where(Dream.user_id != user_id).order_by(Dream.created_at)
+        all_records = (select(self.type_model).where(Dream.user_id != user_id)
                        .offset(offset).limit(limit))
 
         all_records_list = list((await self.session.execute(all_records)).scalars())
