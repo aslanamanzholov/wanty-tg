40111b3888cd84d3293da7c53a0cc91800b83aca
Aslan <fenixx977@gmail.com>
2023-12-08 16:53:02 +0500

    some changes

--- src/db/repositories/dreams.py
+++ src/db/repositories/dreams.py
@@ -52,7 +52,7 @@ class DreamRepo(Repository[Dream]):
             .limit(limit)
         )
 
-        return (await self.session.execute(statement.order_by(Dream.created_at.desc()))).scalars().first()
+        return (await self.session.scalars(statement)).first()
 
     async def get_elements_count_of_dream(self, user_id) -> int:
         """Get dream"""
